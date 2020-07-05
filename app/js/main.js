574722151f9cbbac036f43b898e05ed122dbbd39
Boris Lebedev <global-mangal@yandex.ru>
2020-07-05 18:14:36 +0500

    made full template (exept responsive design)

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,14 @@
-console.log('Main.js is loaded...')
+console.log('main.js is loaded...')
+
+// DropDown Menu Animation
+const menu = document.querySelectorAll('div.best-section__dropdown-single')
+
+menu.forEach((el) => {
+    el.addEventListener('click', () => {
+        if (el.classList.contains('hidden-list')) {
+            el.classList.remove('hidden-list')
+        } else {
+            el.classList.add('hidden-list')
+        }
+    })
+})
